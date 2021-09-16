875a5da1876d850741a74757bb1ffc2dfacbb72d
SamEngComp <salessamuel268@gmail.com>
2021-09-16 11:02:33 -0300

    Initial project setup

---
+++ FOCalendar/FOCalendar.h
@@ -0,0 +1,18 @@
+//
+//  FOCalendar.h
+//  FOCalendar
+//
+//  Created by Samuel Sales on 16/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FOCalendar.
+FOUNDATION_EXPORT double FOCalendarVersionNumber;
+
+//! Project version string for FOCalendar.
+FOUNDATION_EXPORT const unsigned char FOCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FOCalendar/PublicHeader.h>
+
+
